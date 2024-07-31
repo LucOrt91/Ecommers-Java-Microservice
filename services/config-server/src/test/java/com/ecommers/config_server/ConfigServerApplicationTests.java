f80a6dca88f169b3bf0b64685f896520bc9e2e2e
Lucas Ortega <lucasnortega91@gmail.com>
2024-07-31 12:14:06 -0300

    creacion de config-server

---
+++ services/config-server/src/test/java/com/ecommers/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommers.Config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
